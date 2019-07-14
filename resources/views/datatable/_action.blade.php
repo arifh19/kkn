298649d5e2048b21641889bf034382729bca6245
Arif Hidayat <arif.h@mail.ugm.ac.id>
2019-07-15 07:30:41 +0800

    first commit

--- resources/views/datatable/_action.blade.php
+++ resources/views/datatable/_action.blade.php
@@ -4,4 +4,26 @@
     </button>
     {!! Form::submit('Hapus', ['class' => 'btn btn-danger']) !!}
 {!! Form::close() !!}
-
+<html>
+<div class="modal fade" id="modal-default">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <h4 class="modal-title">Dokumentasi</h4>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label>Dokumentasi: </label></br>
+                    <img src="{{ asset('/dokumentasi/'+$model->dokumentasi) }}" width="200" height="200" />
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+          <!-- /.modal-content -->
+    </div>
+        <!-- /.modal-dialog -->
+</div>
+</html>
